a11b45942b4e05e4d890fcba06f006439b1b535a
baronkwan <baronkwan@outlook.com>
2016-03-04 11:58:06 -0800

    add some controller

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,44 @@
-get '/' do
+get '/' do # homepage login page
+  redirect '/login'
+end
+
+get '/login' do # homepage login page
   erb :index
 end
 
-get '/new' do
+
+get '/users/new' do # get to register page
   erb :new
 end
 
-post '/' do
-  #User.create(user: params["username"], password: params["password"])
-  erb :'users/user'
+post '/users/new' do
+  @user = User.create(name: params[:username], email: params[:email], password: params[:password])
+  @message = "You can log in with your account now."
+  erb :index
+end
+
+post '/users/login' do
+  @user = User.find_by(email: params[:email])
+  if @user.authenticate(params[:password])
+    seccsion[:id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @message = "Email and Password not match. Please try again."
+    erb :index
+  end
+
+  # # if User.authenticate(params[:email], params[:password])
+  # "If log in is true, move to user page"
+  #   @user = User.find_by(email: params[:email])
+  #   session[:id] = @user.id
+  #   redirect "/users/#{@user.id}"
+  # else
+  #   @message = "Email and Password not match. Please try again."
+  #   erb :'users/index'
+  # end
+end
+
+post '/' do # Log out
+  session[:id] = nil
+  redirect '/login'
 end
--- app/controllers/user_index.rb
+++ app/controllers/user_index.rb
@@ -1,47 +1,65 @@
 get '/users' do
-#display all user
+  #display all user
+  @users = User.all
   erb :'users/index'
 end
 
 post '/users/login' do
-    # @user = User.find_by(email: params[:email])
-  # if @user.authenticate(params[:password])
-  #   seccsion[id] = @user.id
-  #   "I got in"
-  # else
-  #   "I'm locked out"
-  # end
+  @user = User.find_by(email: params[:email])
+  if @user.authenticate(params[:password])
+    seccsion[:id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @message = "Email and Password not match. Please try again."
+    erb :'users/index'
+  end
 
-  # if User.authenticate(params[:email], params[:password])
-  "If log in is true, move to user page"
+  # # if User.authenticate(params[:email], params[:password])
+  # "If log in is true, move to user page"
   #   @user = User.find_by(email: params[:email])
   #   session[:id] = @user.id
   #   redirect "/users/#{@user.id}"
   # else
-  #   @message = "message"
-  # redirect :'users/:id'
+  #   @message = "Email and Password not match. Please try again."
+  #   erb :'users/index'
+  # end
 end
+
+
 
 get '/users/:id' do
-  @user = User.find(params["id"])
-  erb :'users/users'
+  @user = User.find_by_id(params[:id])
+  if @user # if user exist in the database (not nil)
+    if session[:id] == true # Check if session id is matching with the user id
+      erb :users # This render the content for specific user. Also suggesting to change name to /users/user_homepage
+    else
+      redirect '/' # redirect back to home page if session id doesn't match
+    end
+  else
+    @message = "User not found!" # Create message for what goes wrong for user when returning back to homepage
+    erb :index # Render the index page with the message
+  end
 end
 
-get '/users/:id/edit' do
-  @user = User.find(params["id"])
+get '/users/:id/edit' do # get to the edit form specific user
+  @user = User.find_by_id(params[:id])
   erb :'users/edit'
 end
 
-put '/users/:id' do
-@user = User.find(params["id"])
-@user.assign_attributes(what_we_are_upding: params["thing we updating"])
-if @user.save
-  redirect '/users/index'
-else
-  erb :error
+
+put '/users/:id' do # Update a specific user (action)
+  @user = User.find_by_id(params[:id])
+  # Update the attributes of user's choice
+  @user.assign_attributes(column: params["attributes"])
+  if @user.save
+    redirect '/users/index'
+  else
+    @message = "Error. Please try again." # Create message for what goes wrong for user when returning back to homepage
+    erb :'users/edit'
+  end
 end
 
 delete '/users' do
-  User.find(param[:id]).destroy
+  User.find_by_id(param[:id]).destroy
   redirect '/'
 end
